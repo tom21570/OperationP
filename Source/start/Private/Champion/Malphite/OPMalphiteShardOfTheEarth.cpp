e33026c497166615a01d44be35f706498633c5d2
c_turtle <eastino@gmail.com>
2024-07-19 06:56:36 +0900

    말파이트 q 대지의 파편 코드 중간 저장
    기본 뼈대만 완성 (맞은 상대방은 느려지고 이동속도가 나는 빨라짐)

---
+++ Source/start/Private/Champion/Malphite/OPMalphiteShardOfTheEarth.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Champion/Malphite/OPMalphiteShardOfTheEarth.h"
+#include "Champion/OPChampion.h"
+#include "Animation/AnimInstance.h" // 애니메이션 인스턴스 헤더 포함
+#include "Animation/OPAnimInstance.h"
+#include "Components/CapsuleComponent.h"
+#include "Champion/Malphite/OPMalphite.h"
+#include "Diavolo/OPDiavolo.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AOPMalphiteShardOfTheEarth::OnDamageCollisionBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnDamageCollisionBeginOverlap(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep,
+		SweepResult);
+	if (OtherActor && OtherActor != this && GetOwner() != nullptr && OtherActor != GetOwner())
+	{
+		AOPDiavolo* TestDiavolo = Cast<AOPDiavolo>(OtherActor);
+		AOPMalphite* MalphiteOwner = Cast<AOPMalphite>(GetOwner());
+		if (TestDiavolo)
+		{
+			// 캐릭터 클래스에서 효과를 적용하는 함수 호출
+			MalphiteOwner->ApplySkill_1_Effect(MalphiteOwner, TestDiavolo);
+		}
+		//말파이트 돌에 맞고 디아볼로가 날아가야되면 디아볼로 충격 코드를 넣어줘야함
+		Destroy();
+	}
+}
---
+++ Source/start/Public/Champion/Malphite/OPMalphiteShardOfTheEarth.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile/OPProjectile.h"
+#include "OPMalphiteShardOfTheEarth.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class START_API AOPMalphiteShardOfTheEarth : public AOPProjectile
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnDamageCollisionBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+};
